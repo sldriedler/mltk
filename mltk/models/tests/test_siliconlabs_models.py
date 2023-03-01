769c4930f43b0cdd9bd4d2d195498efb53d04c58
Dan Riedler <driedler@gmail.com>
2023-03-01 15:42:01 -0800

    fixed bug in test

--- mltk/models/tests/test_siliconlabs_models.py
+++ mltk/models/tests/test_siliconlabs_models.py
@@ -4,7 +4,7 @@ from mltk.utils.test_helper import run_model_operation, generate_run_model_param
 
 @pytest.mark.parametrize(*generate_run_model_params())
 def test_keyword_spotting_on_off(op, tflite, build):
-    run_model_operation('keyword_spotting_on_off', op, tflite, build)
+    run_model_operation('keyword_spotting_on_off_v2', op, tflite, build)
 
 @pytest.mark.parametrize(*generate_run_model_params())
 def test_keyword_spotting_mobilenetv2(op, tflite, build):
